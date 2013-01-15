1669a6a4e51cb5d54ee5b747bf219fa77bae1f43
Greg Whitworth <gregory.whitworth@nana.com>
2013-01-15 13:16:56 -0900

    Made the tag now produce a gallery and work with the tags.

--- mod.fbphotos.php
+++ mod.fbphotos.php
@@ -12,10 +12,44 @@ class Fbphotos extends Fbphotos_base {
         parent::__construct();      
     }
 
+    function get_middle_key( $arr )
+    {
+        $count = count($arr);
+        return floor( $count / 2 );
+    }
+
+    function get_image( $arr, $size )
+    {
+        $middle = $this->get_middle_key($arr);
+        $count = (count($arr) - 1);
+
+        switch( $size )
+        {
+            case 'xsmall':
+                $image = $arr[ $count ]->source;
+                break;
+            case 'small':
+                $image = $arr[ ($count - 1) ]->source;
+                break;
+            case 'medium':
+                $image = $arr[ ($middle - 1) ]->source;
+                break;
+            case 'large':
+                $image = $arr[1]->source;
+                break;
+            case 'xlarge':
+                $image = $arr[0]->source;
+                break;
+        }
+        return $image;
+    }
+
     function get_photos()
     {
-        $albums = unserialize( $this->get_setting_value( 'facebook_albums' ) );
-        $limit = intval( $this->EE->TMPL->fetch_param('limit') );
+        $albums     = unserialize( $this->get_setting_value( 'facebook_albums' ) );        
+        $limit      = intval( $this->EE->TMPL->fetch_param('limit') );
+        $size       = $this->EE->TMPL->fetch_param('size');
+        $thumbnail_size  = $this->EE->TMPL->fetch_param('thumbnail');
 
         $variables = array();
 
@@ -24,20 +58,38 @@ class Fbphotos extends Fbphotos_base {
             $limit = 50;
         }
 
+        if( is_null( $size ) || empty( $size )  )
+        {
+            $size = 'medium';
+        }
+
         $i = 0;
         foreach( $albums as $album )
         {
             $photo_data = $this->get_facebook_graph_data( $album, 'photos' );
             foreach( $photo_data->data as $photo ) {
+                $images = $photo->images;
+
                 if( $i === $limit )
                 {
                     break;
                 }
                 else {
-                    $data[] = array(
-                        "source" => $photo->images[5]->source, 
-                        "name" => @$photo->name 
-                    );
+                    if( isset( $thumbnail_size ) && !empty( $thumbnail_size ) )
+                    {
+                        $data[] = array(
+                            "source" => $this->get_image( $images, $size ), 
+                            "name" => @$photo->name,
+                            "thumbnail" => $this->get_image( $images, $thumbnail_size )
+                        );
+                    }
+                    else {
+                        $data[] = array(
+                            "source" => $this->get_image( $images, $size ), 
+                            "name" => @$photo->name
+                        );
+                    }
+
                 }
                 $i++;
             }
